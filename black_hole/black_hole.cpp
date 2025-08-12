ea4ece8788cfead7f5a7a3704a985f67f2adf8b7
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-08-12 18:06:12 -0500

    Initial commit

---
+++ black_hole/black_hole.cpp
@@ -0,0 +1,41 @@
+// black_hole.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <gl/glew.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <vector>
+#include <iostream>
+#define _USE_MATH_DEFINES
+#include <cmath>
+#include <sstream>
+#include <iomanip>
+#include <cstring>
+#include <chrono>
+#include <fstream>
+
+#ifndef M_PI
+#define M_PI 3.14159265358979323846
+#endif
+using namespace glm;
+using namespace std;
+using Clock = std::chrono::high_resolution_clock;
+
+
+int main() {
+
+    return 0;
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
