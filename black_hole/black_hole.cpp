ed83e94618d19460a1b45d46a33b19537fe9b25b
sam-schmitz <116108955+sam-schmitz@users.noreply.github.com>
2025-08-13 15:51:50 -0500

    Added vectors to the final model. I left some different senarios commented out in the main function.

--- black_hole/black_hole.cpp
+++ black_hole/black_hole.cpp
@@ -1,5 +1,6 @@
-// black_hole.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+// black_hole.cpp
+// By: Sam Schmitz
+// Created a 2d black hole by following the tutorial from kavan on YouTube
 
 #include <iostream>
 #include <gl/glew.h>
@@ -230,6 +231,14 @@ void rk4Step(Ray& ray, double dL, double rs) {
 
 
 int main() {
+    // This ray does 3 loops around the black hole before flying off
+    //rays.push_back(Ray(vec2(-1e11, 3.27606302719999999e10), vec2(c, 0.0f)));
+
+    // These rays come from the left side of the screen
+    for (double i = -8; i <= 8; i += 0.3) {
+        rays.push_back(Ray(vec2(-1e11, i * (1e10)), vec2(c, 0.0f)));
+    }
+
     while (!glfwWindowShouldClose(engine.window)) {
         engine.run();
         SagA.draw();
@@ -244,14 +253,3 @@ int main() {
     }
     return 0;
 }
-
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
